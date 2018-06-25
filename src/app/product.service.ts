b1f0d577dc9e13753ac7330a9bd7f1291823d8e4
LakshayAggarwal <lakshayagg29@gmail.com>
2018-06-25 13:32:00 +0530

    product service update

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -7,7 +7,7 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  private _albumUrl='../album/album.json'
+  private _albumUrl='../assets/album.json'
 
   getAlbum(id:number){
     return this._http.get(this._albumUrl).map((response)=>{
